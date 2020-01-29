3373221f7ab849845e27bdff0a69698672c40f7b
Livingstone Asabahebwa <ahebwa49@gmail.com>
2020-01-29 19:21:16 +0300

    add a profile form

--- src/common/Profile.js
+++ src/common/Profile.js
@@ -1,5 +1,6 @@
 import React from "react";
 import { connect } from "react-redux";
+import { Link } from "react-router-dom";
 
 const mapStateToProps = state => {
   return {
@@ -15,8 +16,105 @@ class Profile extends React.Component {
 
   render() {
     return (
-      <div>
-        <h1>Profile page for {this.props.user.username}</h1>
+      <div className="page-wrapper">
+        <div className="container feature-top pb-0">
+          <div className="form-wrapper">
+            <div className="loading">loading ...</div>
+            <h1>Profile</h1>
+            <form className="form" onSubmit={this.handleSubmit} method="post">
+              <div className="form-group">
+                <label htmlFor="email" className="form-label">
+                  Your Email
+                </label>
+                <input
+                  type="text"
+                  id="email"
+                  name="email"
+                  value={this.props.user.username}
+                  className="form-input"
+                />
+              </div>
+              <div className="form-group">
+                <label htmlFor="phone number" className="form-label">
+                  Phone number
+                </label>
+                <input
+                  type="text"
+                  id="password"
+                  name="password"
+                  value={this.state.password}
+                  className="form-input"
+                />
+              </div>
+              <div className="form-group">
+                <label htmlFor="address" className="form-label">
+                  Address
+                </label>
+                <input
+                  type="text"
+                  id="password"
+                  name="password"
+                  value={this.state.password}
+                  className="form-input"
+                />
+              </div>
+              <div className="form-group">
+                <label htmlFor="date of birth" className="form-label">
+                  Date of birth
+                </label>
+                <input
+                  type="text"
+                  id="password"
+                  name="password"
+                  value={this.state.password}
+                  className="form-input"
+                />
+              </div>
+              <div className="form-group">
+                <label htmlFor="security questions" className="form-label">
+                  Security questions
+                </label>
+                <br />
+                <label htmlFor="security questions" className="form-label">
+                  What is your childhood nickname?
+                </label>
+                <input
+                  type="text"
+                  id="password"
+                  name="password"
+                  value={this.state.password}
+                  className="form-input"
+                />
+                <br />
+                <label htmlFor="security questions" className="form-label">
+                  What is your favourite book?
+                </label>
+                <input
+                  type="text"
+                  id="password"
+                  name="password"
+                  value={this.state.password}
+                  className="form-input"
+                />
+                <br />
+                <label htmlFor="security questions" className="form-label">
+                  Where did you meet your spouse?
+                </label>
+                <input
+                  type="text"
+                  id="password"
+                  name="password"
+                  value={this.state.password}
+                  className="form-input"
+                />
+              </div>
+              <div className="errorMessage">{this.state.error}</div>
+              <button type="submit" className="form-button">
+                Edit
+              </button>
+            </form>
+          </div>
+        </div>
       </div>
     );
   }
